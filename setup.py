26f84ebba275f726c1400d5f4684493753aaee33
mglagola <mark.glagola@gmail.com>
2013-04-24 07:20:44 -0500

    update setup.py - database settings added

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ def find_replace_all(find, replace, file_paths):
 		print line,
 
 def main():
+	print '\nMake sure you enter "appropriate" responses to each question, DO NOT enter shell commands. Anything that happens is not my fault.\n'
 	project_name = raw_input('What would you like to name your project? ')
 
 	settings = os.path.join(CURRENT_DIR, 'project_name', 'project_name', 'settings.py')
@@ -27,7 +28,15 @@ def main():
 	os.rename(CURRENT_DIR + '/project_name/project_name', CURRENT_DIR + '/project_name/%s' % project_name)
 	os.rename(CURRENT_DIR + '/project_name', CURRENT_DIR + '/%s' % project_name)
 
-	#rename folders, procfile, and everything in settings.py and manage.py
+	database_name = raw_input('What should your database name be? ')
+	log('Creating postgres database %s using cmd "createdb"' % database_name)
+	subprocess.call('createdb ' + database_name, shell=True)
+	file_paths = [os.path.join(CURRENT_DIR, project_name, project_name, 'settings.py')]
+	find_replace_all('POSTGRES_DATABASE_NAME', database_name, file_paths)
+	log('Settings postgres database name to %s in settings.py' % database_name)
 
+	user_name = raw_input('What is your current computer username (i.e. Mark)? ')
+	log('Settings postgres username to ' + user_name)
+	find_replace_all('POSTGRES_USER_NAME', user_name, file_paths)
 
 main()

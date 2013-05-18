abbdbe873e6a85a2827be4a550a48b36d07f2d71
mglagola <mark.glagola@gmail.com>
2013-05-18 18:31:44 -0500

    updated how logs are printed + update to raw_input format

--- setup.py
+++ setup.py
@@ -3,10 +3,13 @@ import os, subprocess, fileinput
 CURRENT_DIR = os.getcwd()
 
 def log(message):
-	print '===> ' + message + '...'
+	print message + '...'
+
+def get_input(message):
+	return raw_input('==> ' + message)
 
 def ask(message):
-	response = raw_input(message + ' (y/n) ')
+	response = raw_input('==> ' + message + ' (y/n) ')
 	if response == 'y':
 		return True
 	return False
@@ -40,14 +43,14 @@ def create_static_folder(project_name):
 	subprocess.call('mkdir %s/%s/static' % (project_name, project_name), shell=True)
 
 def create_local_database(project_name):
-	database_name = raw_input('What should your database name be for local testing? ')
+	database_name = get_input('What should your database name be for local testing? ')
 	log('Creating postgres database %s using cmd "createdb"' % database_name)
 	subprocess.call('createdb ' + database_name, shell=True)
 	file_paths = [os.path.join(CURRENT_DIR, project_name, project_name, 'settings.py')]
 	find_replace_all('POSTGRES_DATABASE_NAME', database_name, file_paths)
 	log('Settings postgres database name to %s in settings.py' % database_name)
 
-	user_name = raw_input('What is your home folder name (i.e. Mark)? ')
+	user_name = get_input('What is your home folder name (i.e. Mark)? ')
 	log('Settings postgres username to ' + user_name)
 	find_replace_all('POSTGRES_USER_NAME', user_name, file_paths)
 
@@ -60,10 +63,10 @@ def create_herokuapp():
 def s3_setup(project_name):
 	log ('Initializing Amazon S3 Setup')
 	log ('Enter your Amazon S3 Keys and bucket name')
-	AWS_ACCESS_KEY_ID = raw_input('AWS ACCESS KEY ID: ')
-	AWS_SECRET_ACCESS_KEY = raw_input('AWS SECRET ACCESS KEY: ')
-	S3_BUCKET_NAME = raw_input('S3 BUCKET NAME: ')
-	venv_name = raw_input('What is the name of your virtualenv dir? (i.e. ".env"): ')
+	AWS_ACCESS_KEY_ID = get_input('AWS ACCESS KEY ID: ')
+	AWS_SECRET_ACCESS_KEY = get_input('AWS SECRET ACCESS KEY: ')
+	S3_BUCKET_NAME = get_input('S3 BUCKET NAME: ')
+	venv_name = get_input('What is the name of your virtualenv dir? (i.e. ".env"): ')
 	log('Setting Heroku Config')
 	subprocess.call(
 		'heroku config:set AWS_ACCESS_KEY_ID=%s AWS_SECRET_ACCESS_KEY=%s S3_BUCKET_NAME=%s' 
@@ -82,19 +85,16 @@ def s3_setup(project_name):
 def main():
 	print '\nMake sure you enter "appropriate" responses to each question, DO NOT enter shell commands. Anything that happens is not my fault.\n'
 
-	project_name = raw_input('What would you like to name your project? ')
+	project_name = get_input('What would you like to name your project? ')
 	rename_project(project_name)
 
 	create_static_folder(project_name)
-	print '\n'
 
 	# if ask('Would you like to create a local postgres database?'):
 	create_local_database(project_name)
-	print '\n'
 
 	# if ask('Would you like to create your heroku app now?'):
 	create_herokuapp()
-	print '\n'
 
 		# if ask('Would you like to setup S3 for your heroku app?'):
 	s3_setup(project_name)
